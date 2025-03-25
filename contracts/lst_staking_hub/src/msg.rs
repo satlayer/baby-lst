f1ac958d1f38cd23261b0a3ee87b59156f4cec60
Night Owl <nightowl121@protonmail.com>
2025-03-25 13:25:57 +0545

    chore: remove unused files in staking hub

--- contracts/lst_staking_hub/src/msg.rs
+++
@@ -1,9 +0,0 @@
-use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::ValidatorResponse;
-
-#[cw_serde]
-#[derive(QueryResponses)]
-pub enum QueryValidators {
-    #[returns(Vec<ValidatorResponse>)]
-    GetValidatorsForDelegation {},
-}
