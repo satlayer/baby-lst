fe04122851be179e670b0596a771f87ed29a64da
VM <112189277+sysvm@users.noreply.github.com>
2025-04-12 17:26:51 +0800

    chore: add unit test for lst_validators_registry (#65)
    Co-authored-by: VM <arimas@foxmail.com>

--- contracts/lst_validators_registry/src/helper.rs
+++ contracts/lst_validators_registry/src/helper.rs
@@ -17,41 +17,46 @@ pub(crate) fn convert_addr_by_prefix(address: &str, prefix: &str) -> String {
     bech32::encode::<Bech32>(hrp, &data).expect("Failed to encode address")
 }
 
-#[test]
-fn test_account_to_validator_addr() {
-    let val_account_addr = "bbn109x4ruspxarwt62puwcenhclw36l9v7jcgrj48";
-    let converted_val_addr = convert_addr_by_prefix(val_account_addr, VALIDATOR_ADDR_PREFIX);
-    assert_eq!(
-        converted_val_addr,
-        "bbnvaloper109x4ruspxarwt62puwcenhclw36l9v7j92f0ex"
-    );
-}
+#[cfg(test)]
+mod tests {
+    use super::{convert_addr_by_prefix, VALIDATOR_ADDR_PREFIX};
 
-#[test]
-fn test_same_prefix() {
-    let bbn_addr = "bbnvaloper109x4ruspxarwt62puwcenhclw36l9v7j92f0ex";
+    #[test]
+    fn test_account_to_validator_addr() {
+        let val_account_addr = "bbn109x4ruspxarwt62puwcenhclw36l9v7jcgrj48";
+        let converted_val_addr = convert_addr_by_prefix(val_account_addr, VALIDATOR_ADDR_PREFIX);
+        assert_eq!(
+            converted_val_addr,
+            "bbnvaloper109x4ruspxarwt62puwcenhclw36l9v7j92f0ex"
+        );
+    }
 
-    let result = convert_addr_by_prefix(bbn_addr, VALIDATOR_ADDR_PREFIX);
-    assert_eq!(bbn_addr, result);
-}
+    #[test]
+    fn test_same_prefix() {
+        let bbn_addr = "bbnvaloper109x4ruspxarwt62puwcenhclw36l9v7j92f0ex";
 
-#[test]
-#[should_panic]
-fn test_invalid_address() {
-    convert_addr_by_prefix("invalid_addr", VALIDATOR_ADDR_PREFIX);
-}
+        let result = convert_addr_by_prefix(bbn_addr, VALIDATOR_ADDR_PREFIX);
+        assert_eq!(bbn_addr, result);
+    }
 
-#[test]
-#[should_panic]
-fn test_invalid_prefix() {
-    let address = "bbnvaloper109x4ruspxarwt62puwcenhclw36l9v7j92f0ex";
-    convert_addr_by_prefix(address, "");
-}
+    #[test]
+    #[should_panic]
+    fn test_invalid_address() {
+        convert_addr_by_prefix("invalid_addr", VALIDATOR_ADDR_PREFIX);
+    }
 
-#[test]
-fn test_multiple_conversion() {
-    let original = "bbn109x4ruspxarwt62puwcenhclw36l9v7jcgrj48";
-    let to_bbn_valoper = convert_addr_by_prefix(original, VALIDATOR_ADDR_PREFIX);
-    let back_bbn = convert_addr_by_prefix(&to_bbn_valoper, "bbn");
-    assert_eq!(original, back_bbn);
+    #[test]
+    #[should_panic]
+    fn test_invalid_prefix() {
+        let address = "bbnvaloper109x4ruspxarwt62puwcenhclw36l9v7j92f0ex";
+        convert_addr_by_prefix(address, "");
+    }
+
+    #[test]
+    fn test_multiple_conversion() {
+        let original = "bbn109x4ruspxarwt62puwcenhclw36l9v7jcgrj48";
+        let to_bbn_valoper = convert_addr_by_prefix(original, VALIDATOR_ADDR_PREFIX);
+        let back_bbn = convert_addr_by_prefix(&to_bbn_valoper, "bbn");
+        assert_eq!(original, back_bbn);
+    }
 }
