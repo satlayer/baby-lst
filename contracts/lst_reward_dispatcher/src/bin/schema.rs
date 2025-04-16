846f445e5603a5488800af4a2873356b2bee9bbd
Fuxing <loh@fuxing.dev>
2025-04-16 18:57:21 +0800

    chore: #[cfg(not(tarpaulin_include))]

--- contracts/lst_reward_dispatcher/src/bin/schema.rs
+++ contracts/lst_reward_dispatcher/src/bin/schema.rs
@@ -2,6 +2,7 @@ use cosmwasm_schema::write_api;
 
 use lst_common::rewards_msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
+#[cfg(not(tarpaulin_include))]
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
--- contracts/lst_token/src/bin/schema.rs
+++ contracts/lst_token/src/bin/schema.rs
@@ -3,6 +3,7 @@ use cosmwasm_schema::write_api;
 use cw20_base::msg::{ExecuteMsg, QueryMsg};
 use lst_token::msg::InstantiateMsg;
 
+#[cfg(not(tarpaulin_include))]
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
